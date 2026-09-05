b9e4b95e0960b941e998a0422761b5b96817b0df
iNetBar <ibar@qq.com>
2026-09-06 00:16:39 +0800

    Add files via upload

--- main.ts
+++ main.ts
@@ -17,7 +17,7 @@ import { Hono } from "hono";
 // =====================================================================
 
 // ---------- ENV ----------
-const VERSION = "2.3.8";
+const VERSION = "2.3.9";
 const ENV = {
   PROXY_KEY: (Deno.env.get("PROXY_KEY") || "").split(",").map((s) => s.trim()).filter(Boolean),
   SEED_KEYS: Deno.env.get("SEED_KEYS") || "",
